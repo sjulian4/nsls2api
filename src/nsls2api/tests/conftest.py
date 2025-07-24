f7168cf98a6e3167c1fa15a33b7b89171000af7f
sjulian4 <sarah.julian@stonybrook.edu>
2025-07-24 11:33:20 -0400

    removing data admin group

--- src/nsls2api/tests/conftest.py
+++ src/nsls2api/tests/conftest.py
@@ -56,7 +56,7 @@ async def db():
         facility_id="nsls2",
         fullname="National Synchrotron Light Source II",
         pass_facility_id="NSLS-II",
-        data_admin_group="nsls2-data-admins",
+        # data_admin_group="nsls2-data-admins",
         data_admins=["testy-mcdata"],
     )
     await facility.insert()
